375ebdcf6021dac7ee7d35fa5b72a5e706b95d42
thanaphonpra <73868882+thanaphonpra@users.noreply.github.com>
2021-08-05 22:06:08 +0700

    Add files via upload

---
+++ jsdoc2md/TEMPLATE.md
@@ -0,0 +1,75 @@
+# Salesforce Marketing Cloud Cordova Plugin
+
+Use this plugin to implement the Marketing Cloud MobilePush SDK for your [iOS](https://salesforce-marketingcloud.github.io/MarketingCloudSDK-iOS/) and [Android](http://salesforce-marketingcloud.github.io/MarketingCloudSDK-Android/) applications.
+
+## Release Notes
+
+Release notes for the plugin can be found [here](CHANGELOG.md)
+
+## Installation
+
+#### 1. Add plugin to your application via [npm](https://www.npmjs.com/package/cordova-plugin-marketingcloudsdk)
+
+```shell
+cordova plugin add cordova-plugin-marketingcloudsdk
+```
+
+**Required for iOS**
+
+You must have [Cocoapods](https://cocoapods.org/) installed for the iOS platform integration.  Execute the following commands from terminal to install:
+```shell
+sudo gem install cocoapods
+pod repo update
+```
+
+#### 2. Modify your application's `config.xml` to configure the plugin <a name="config"></a>
+
+```xml
+<!-- Required -->
+<preference name="com.salesforce.marketingcloud.app_id" value="{Marketing Cloud application id}" />
+<preference name="com.salesforce.marketingcloud.access_token" value="{Marketing Cloud access token}" />
+<preference name="com.salesforce.marketingcloud.tenant_specific_endpoint" value="{URL retrieved from Marketing Cloud adminstration page}" />
+
+<!-- Required - Android Only -->
+<platform name="android">
+  <preference name="com.salesforce.marketingcloud.notification_small_icon" value="ic_notification" />
+</platform>
+
+<!-- Optional -->
+<preference name="com.salesforce.marketingcloud.analytics" value="{true|false}" />
+<preference name="com.salesforce.marketingcloud.delay_registration_until_contact_key_is_set" value="{true|false}" />
+```
+
+#### 3. Provide FCM credentials
+
+To enable push support for the Android platform you will need to include the google-services.json file.  
+
+1. Download the file from your application's [Firebase console](https://console.firebase.google.com/) and place it in your project's root folder.  
+2. Add following to Android element in your `config.xml`:
+
+```xml
+<platform name="android">
+  <resource-file src="google-services.json" target="app/google-services.json" />
+</platform>
+```
+
+#### 4. Enable iOS Push
+
+Follow [these instructions](./ios_push.md) to enable push for iOS.
+
+## API Reference <a name="reference"></a>
+
+{{#orphans~}}
+{{>member-index}}
+{{/orphans}}
+
+---
+
+{{#modules~}}
+{{>header~}}
+{{>body~}}
+{{>members~}}
+
+---
+
+{{/modules}}
---
+++ src/ios/AppDelegate+MCCordovaPlugin.m
@@ -0,0 +1,105 @@
+// AppDelegate+MCCordovaPlugin.m
+//
+// Copyright (c) 2018 Salesforce, Inc
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer. Redistributions in binary
+// form must reproduce the above copyright notice, this list of conditions and
+// the following disclaimer in the documentation and/or other materials
+// provided with the distribution. Neither the name of the nor the names of
+// its contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#import <objc/runtime.h>
+#import "AppDelegate+MCCordovaPlugin.h"
+#import "MCCordovaPlugin.h"
+
+#import "MarketingCloudSDK/MarketingCloudSDK.h"
+
+@implementation AppDelegate (MCCordovaPlugin)
+
+- (void)sfmc_setNotificationDelegate {
+    if (@available(iOS 10, *)) {
+        [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+    }
+}
+
+- (void)application:(UIApplication *)application
+    didReceiveRemoteNotification:(NSDictionary *)userInfo
+          fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    if (@available(iOS 10, *)) {
+        UNMutableNotificationContent *theSilentPushContent =
+            [[UNMutableNotificationContent alloc] init];
+        theSilentPushContent.userInfo = userInfo;
+        UNNotificationRequest *theSilentPushRequest =
+            [UNNotificationRequest requestWithIdentifier:[NSUUID UUID].UUIDString
+                                                 content:theSilentPushContent
+                                                 trigger:nil];
+
+        [[MarketingCloudSDK sharedInstance] sfmc_setNotificationRequest:theSilentPushRequest];
+    } else {
+        [[MarketingCloudSDK sharedInstance] sfmc_setNotificationUserInfo:userInfo];
+    }
+    completionHandler(UIBackgroundFetchResultNewData);
+}
+
+- (void)application:(UIApplication *)application
+    didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    // save the device token
+    [[MarketingCloudSDK sharedInstance] sfmc_setDeviceToken:deviceToken];
+}
+
+- (void)application:(UIApplication *)application
+    didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    os_log_debug(OS_LOG_DEFAULT, "didFailToRegisterForRemoteNotificationsWithError = %@", error);
+}
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+    didReceiveNotificationResponse:(UNNotificationResponse *)response
+             withCompletionHandler:(void (^)(void))completionHandler {
+    // tell the MarketingCloudSDK about the notification
+    [[MarketingCloudSDK sharedInstance] sfmc_setNotificationRequest:response.notification.request];
+
+    if (completionHandler != nil) {
+        completionHandler();
+    }
+}
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+       willPresentNotification:(UNNotification *)notification
+         withCompletionHandler:
+             (void (^)(UNNotificationPresentationOptions options))completionHandler {
+    if (completionHandler != nil) {
+        completionHandler(UNNotificationPresentationOptionAlert);
+    }
+}
+
+#pragma mark - Custom message handlers
+
+- (void)notificationReceivedWithUserInfo:(NSDictionary *)userInfo
+                             messageType:(NSString *)messageType
+                               alertText:(NSString *)alertText {
+    NSLog(@"### USERINFO: %@", userInfo);
+    NSLog(@"### alertText: %@", alertText);
+
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"kCDVPushReceivedNotification"
+                                                        object:self
+                                                      userInfo:nil];
+}
+
+@end
